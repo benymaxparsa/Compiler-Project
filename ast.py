90a84e515b2cef78bf8965ffbf9732af011f246f
Parsa KamaliPour <parsakamalipour@gmail.com>
2021-06-15 21:00:53 +0430

    Added Abstract Syntax Tree

---
+++ ast.py
@@ -0,0 +1,30 @@
+class Number:
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self):
+        return int(self.value)
+
+
+class BinaryOperator:
+    def __init__(self, left, right):
+        self.right = right
+        self.left = left
+
+
+class Sum(BinaryOperator):
+    def eval(self):
+        return self.left.eval() + self.right.eval()
+
+
+class Sub(BinaryOperator):
+    def eval(self):
+        return self.left.eval() - self.right.eval()
+
+
+class Print():
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self):
+        print(self.value.eval())
